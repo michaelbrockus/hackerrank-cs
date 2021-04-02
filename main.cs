a430a65c8155c3aadb0b4fed9c88a4628ffaf89b
Michael <michaelbrockus@gmail.com>
2021-04-01 18:37:15 -0700

    first commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+//
+// file: main.cs
+// author: Michael Brockus
+// gmail: <michaelbrockus@gmail.com>
+//
+using System;
+
+
+public class Prog
+{
+    //
+    // main is where program execution starts
+    static int Main(String[] args)
+    {
+      Console.WriteLine("Lets do some HackerRank stuff");
+      return 0;
+    } // end of method main
+
+} // end of class
